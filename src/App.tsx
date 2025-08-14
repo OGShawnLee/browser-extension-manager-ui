6c0dab9e001ec1255380378140eef15972437a2f
Shawn <changoloco.evg@gmail.com>
2025-08-14 18:05:41 +0000

    feat: add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ function App() {
     <main className="max-w-6xl mx-auto p-4 flex flex-col items-center gap-8">
       <GUIHeader />
       <GUIExtensionList />
+      <GUIAttribution />
     </main>
   )
+}
+
+function GUIAttribution() {
+  return (
+    <footer>
+      <p className="text-center text-sm text-neutral-500 dark:text-neutral-400">
+        Made with ❤️ by {" "}
+        <a className="font-bold text-neutral-900 dark:text-white" href="https://frontendmentor.io/profile/OGShawnLee" target="_blank">
+          Shawn Lee
+        </a>
+      </p>
+    </footer>
+  );
 }
 
 export default App
